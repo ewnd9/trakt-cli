ebcd829e9736705d8432c5aa06bfdb9b00ba4af5
ewnd9 <ewndnine@gmail.com>
2016-02-23 18:38:24 +0800

    add missing meow and inquirer-bluebird dependencies, add tests

--- test/index-spec.js
+++ test/index-spec.js
@@ -1,6 +1,8 @@
 import test from 'ava';
-import 'babel-core/register';
+import path from 'path';
+import { execSync } from 'child_process';
 
 test('true', t => {
-  t.true(true);
+  const output = execSync(path.resolve(__dirname, '..', 'cli.js')).toString();
+  t.regexTest(/(access trakt from CLI|copy pin from)/, output);
 });
